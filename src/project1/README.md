070c9fe7875ffdaaf8ea47700c496195278aa674
frillecode <frihae25@gmail.com>
2021-05-21 13:33:01 +0200

    project1

--- src/project1/README.md
+++ src/project1/README.md
@@ -12,13 +12,19 @@ Using a text corpus found on the cds-language GitHub repo or a corpus of your ow
 - Use this to calculate mutual information between the target word and all collocates across the corpus
 - Save result as a single file consisting of three columns: collocate, raw_frequency, MI
 
-__Bonus challenge__  
-- Use argparse to take inputs from the command line as parameters
-
 
 ## Methods
- 
+For this project, I used a dataset consisting of a corpus of 100 English novels, covering the 19th and the beginning of the 20th century. I created a .py-script which takes this corpus, preprocesses the text (remove punctuation, etc.), and calculates collocates for a specific target word. To get a measure of the strength of the association between the target word and the collocates, I calculated the Mutual Information (MI) between them. MI is extracted through calculations of observed and expected frequencies (Evert, 2014):  
+MI is given by: _MI = log(O11/E11)_   
+, where u is the target word and v is the collocate. 
 
+<p align="center">
+    <img src="../../figures/project1_contingencytable.png" width="400" height="200">
+  <p>
+
+The window size and target word can be specified through the command-line. 
+
+  
 ## Usage
 The structure of the repository belonging to this assignment is as follows:  
   - Data: _../../data/project1/_ 
@@ -52,11 +58,7 @@ $ cd src/project1
 $ python3 collocation.py
 ```
 
-
-
-
-
-????????
+To specify a keyword and window size
 
 You can get more information on which optional arguments can be parsed by running:
 ``` bash
@@ -65,6 +67,10 @@ $ python3 edge_detection.py --help
 
 ## Discussion of results
 The resulting output-files from running the script can be found in 'out/'. 
+
+
+## References
+Evert, S., (2004), "Association Measures", http://collocations.de/AM/index.html, [Retrieved May 21 2021]
 
 
 
--- src/project1/collocation.py
+++ src/project1/collocation.py
@@ -4,6 +4,7 @@
 import os
 import re
 import string
+import argparse
 import numpy as np
 from os import listdir
 from pathlib import Path
@@ -11,87 +12,127 @@ import pandas as pd
 from collections import Counter
 
 
-# Define function for calculating MI
-def collocate_function(filepath, keyword, window_size):
-    
-    all_texts = []
-    
-    # Load files from directory
-    for filename in Path(filepath).glob("*.txt"):
-        with open(filename, "r", encoding="utf-8") as file:
-            text = file.read()
-            all_texts.append(text)
+class Collocation:
+    """A class for calculating Mutual Information (MI) with collocates given a target word and a window size. The class contains functions for loading and preprocessing data, extracting relevant information, and calculating MI. A dataframe with collocate, raw_frequency, and MI is returned. 
+    """
+    def __init__(self, args):
+        self.args = args
+       
+        
+    # Define function for calculating MI
+    def preprocessing(self, filepath):
+        """Function for loading in text data from folder, creating a corpus of all texts, and preprocessing it. 
+        Input:
+            filepath: str, path to folder with text data
+        """
+        all_texts = []
 
-    corpus = " ".join(all_texts)
-    
-    corpus = re.sub(r"\W+", " ", corpus) #remove punctuation
-    corpus = corpus.lower() #lowercase
+        # Load files from directory
+        for filename in Path(filepath).glob("*.txt"):
+            with open(filename, "r", encoding="utf-8") as file:
+                text = file.read()
+                all_texts.append(text)
+                
+        # Create corpus from all texts
+        self.corpus = " ".join(all_texts)
+        
+        # Data cleaning
+        self.corpus = re.sub(r"\W+", " ", self.corpus) #remove punctuation
+        self.corpus = self.corpus.lower() #lowercase
+        self.tokenized = [token for token in self.corpus.split()] #tokenizing - splitting by whitespaces
 
-    tokenized = [token for token in corpus.split()] #tokenizing - splitting by whitespaces
+    def collocations(self):
+        """Function for calculating collocations given target word and a window size
+        """
 
-    counter_object = Counter(tokenized) #returns number of time each element, i.e. collocate, appears in list      
-
-    u = counter_object.get(keyword)
-
-    collocates = []
-    
-    for word_n in range(len(tokenized)):
-
-        if tokenized[word_n] == keyword:
-
-            index_value = word_n
-
-            left_window = max(0, index_value - window_size) #2 words on left side
-            right_window = index_value + window_size + 1 #2 words on right side
-
-            window_list = tokenized[left_window : right_window]
-
-            for word in window_list:
-                if word == keyword:
-                    pass
-                else:
-                    collocates.append(word) #save word             
-
-    collocate = [x for x in Counter(collocates).keys()] #extracting collocates from dictionary to a list
-
-    O11 = [x for x in Counter(collocates).values()] #n times collcoate 
-
-    O12 = [x1 - x2 for (x1, x2) in zip(([u] * len(O11)), O11)]
-
-    R1 = [x1 + x2 for (x1, x2) in zip(O11, O12)]
-
-    C1 = [counter_object.get(w) for w in collocate] #raw frequency
-
+        # Looping through all occurences of target word in the corpus
+        self.collocates = []
+        for word_n in range(len(self.tokenized)): 
+            # When target word appears, use index value to create window
+            if self.tokenized[word_n] == self.args['keyword']:
+                index_value = word_n #save index value of target word
+                left_window = max(0, index_value - self.args['window_size']) #2 words on left side
+                right_window = index_value + self.args['window_size'] + 1 #2 words on right side
+                
+                # Create list of collocates for this occurence of the target word
+                window_list = self.tokenized[left_window : right_window]
+                
+                # Save words into collocate-list for all occurences of target word
+                for word in window_list:
+                    if word == self.args['keyword']: #ensure that target word does not appear with itself
+                        pass
+                    else:
+                        self.collocates.append(word) #save word   
+                        
+        self.collocate = [x for x in Counter(self.collocates).keys()] #extracting collocates from dictionary to a list
 
 
-    # length of text
-    N = len(tokenized)
+    def mutual_information(self):
+        """This function calculates Mutual Information between a target word and its collocates
+        """
+        
+        # Calculating observed frequencies
+        counter_object = Counter(self.tokenized) #returns number of time each element appears in list
+        
+        u = counter_object.get(self.args['keyword']) #extracting count of target word
+        
+        O11 = [x for x in Counter(self.collocates).values()] #occurence of target word with collocate
 
-    # Expected
-    E11 = [x1 * x2 for (x1, x2) in zip(R1, C1)]
-    E11 = [x1 / x2 for (x1, x2) in zip(E11, ([N]*len(E11)))]
+        O12 = [x1 - x2 for (x1, x2) in zip(([u] * len(O11)), O11)] #occurence of target word without collocate
 
-    # return MI
-    MI = [np.log(x1/x2) for (x1, x2) in zip(O11, E11)]
+        R1 = [x1 + x2 for (x1, x2) in zip(O11, O12)] #raw frequency of target word
+        
+        C1 = [counter_object.get(w) for w in self.collocate] #raw frequency of collocate
 
-    df = pd.DataFrame()
-    df["collocate"] = collocate
-    df["raw_frequency"] = C1
-    df["MI"] = MI
-    
-    return df
+        N = len(self.tokenized) #length of text
+
+        # Calculating expected frequencies
+        E11 = [x1 * x2 for (x1, x2) in zip(R1, C1)] 
+        E11 = [x1 / x2 for (x1, x2) in zip(E11, ([N]*len(E11)))] 
+
+        # Calculate MI based on expected and observed co-occurences
+        MI = [np.log(x1/x2) for (x1, x2) in zip(O11, E11)]
+
+        # Save information in dataframe
+        df = pd.DataFrame({"collocate": self.collocate,
+                           "raw_frequency": C1,
+                           "MI": MI})
+        return df
 
 # Define main function
 def main():
-    # Run collocate-function (here I use the keyword "father" and a windowsize of 2)
-    df = collocate_function(filepath = os.path.join("..", "..", "data", "assignment1+2", "100_english_novels", "corpus"), 
-                       keyword = "father", 
-                       window_size = 2)
+    ap = argparse.ArgumentParser(description="[INFO] This script takes text data and calculates Mutual Information scores between collocates and a given target word. The window size can be defined.")
+    # Argument for specifying pixel values for resizing images
+    ap.add_argument("-w", 
+                "--window_size", 
+                required=False, 
+                type=int,
+                default=2,
+                help="int, window size for co-occurence (e.g 2 means 2 words before + 2 words after target word)") 
+    # Argument for specifying number of neurons in hidden layer
+    ap.add_argument("-k",  
+                "--keyword", 
+                required=False, 
+                type=str, 
+                default="father", 
+                help="str, target word")
+
+    
+    args = vars(ap.parse_args())
+    
+    
+    # Runnning analysis
+    col = Collocation(args) #class object
+    col.preprocessing(filepath = os.path.join("..", "..", "data", "project1", "100_english_novels", "corpus")) #preprocessing
+    col.collocations() #calculating frequencies
+    df = col.mutual_information() #calculating MI
     
     # Save file
     outpath = os.path.join("out", "collocates.csv")
-    df.to_csv(outpath)
+    df.to_csv(outpath, index=False)
     
 # Define behaviour when called from command line
 if __name__=="__main__":
+    print(f"[INFO] Calculating....")
     main()
+    print(f"[INFO] DONE! You can now find the complete results in the 'out/'-folder")
